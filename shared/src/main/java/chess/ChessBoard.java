bcba53925ae4fc1030db44c7365d61470c5f0cb0
Efrain Cuessy <efraincuessy@gmail.com>
2026-09-15 11:41:27 -0600

    feat(chessPosition): implemeted position with equals, hashCode, and toString

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,6 +8,8 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board = new ChessPiece[8][8];
+
     public ChessBoard() {
         
     }
@@ -19,6 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
+        board[position.getRow()][position.getColumn() - 1] = piece;
         throw new RuntimeException("Not implemented");
     }
 
