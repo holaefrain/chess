cef278e59f623b397b567e38b23181e9dd05ccff
Efrain Cuessy <efraincuessy@gmail.com>
2026-09-19 19:22:01 -0600

    feat(chessPiece): add fields, constructor, and movement direction constants

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -10,7 +12,40 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
+    /** Whether this piece has moved during the current game. Used by ChessGame to decide castling privleges. Excluded from equals/hashCode so that two boards with the same pieces in the same squares are equal */
+    private boolean hasMoved = false;
+
+    // Bishop directions of travel, as pairs
+    private static final int[][] DIAGONALS = {
+        {1,1}, {1,-1}, {-1,1}, {-1,-1}
+    };
+
+    // Rook directions of travel
+    private static final int[][] STRAIGHTS = {
+        {1,0}, {-1, 0}, {0, 1}, {0, -1}
+    };
+
+    // Queen + King directions of travel
+    private static final int[][] ALL_DIRECTIONS = {
+        {1, 1}, {1, -1}, {-1, 1}, {-1, -1}, {1, 0}, {-1, 0}, {0, 1},{0, -1}
+    };
+
+    // Knight directions of travel; offsets the jump
+    private static final int[][] KNIGHT_OFFSETS = {
+        {2,1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+    };
+
+    // Pawn promotion; the pieces it can promote to
+    private static final PieceType[] PROMOTION_TYPES = {
+        PieceType.QUEEN, PieceType.ROOK, PieceType.BISHOP, PieceType.KNIGHT
+    };
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
